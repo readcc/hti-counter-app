837bf28bcc9895fca6a613183c759c3adbcf6b36
Clinton Read <readcc@gmail.com>
2024-04-01 19:25:01 +0000

    CodeSandbox Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './hti-logo500.png';
-import './App.css';
-import CounterApp from './component/CounterApp';
-import { Provider } from 'react-redux';
-import store from './store';
+import logo from "./hti-logo500.png";
+import "./App.css";
+import CounterApp from "./component/CounterApp";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Provider store={store}>
-        <CounterApp />
+          <CounterApp />
         </Provider>
       </header>
     </div>
